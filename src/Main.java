1ae5a96521e3079b0b31a20238ed553d286f4758
rodgreg02 <mindera@ME00391.local>
2023-11-10 09:06:24 +0000

    Added bunch of func

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        CPU cpu = new CPU();
+        System.out.println("What's your username?");
+        Player player = new Player(scanner.next());
+
+
+        Playfield playfield = new Playfield();
+        playfield.createField();
+        playfield.drawField();
+
+
     }
 }
