bc9b848818c96f599d7c24100adbfd26c0ab0514
Fátima <fatimaalvesa10@gmail.com>
2023-11-13 12:30:23 +0000

    update 2

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,18 @@ public class Main {
         playfield.createField();
         playfield.drawField();
 
+
         int i = 0;
+        while (!gameOver) {
 
+            Scanner scanner0 = new Scanner(System.in);
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("\nInsert coordinates to place ship, and the end coordinates for the ship.");
+            System.out.println("The ship you,re setting needs " + player.playerBoats[i].size + " spaces");
+            playfield.placeShip(player.setShips(player.playerBoats, scanner0.next(), scanner2.next(), i));
+            i++;
+            playfield.drawField();
+        }
 
     }
-    }
+}
